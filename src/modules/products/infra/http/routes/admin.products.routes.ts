e0470aed215728e2d693adfc510e6e27de78cc9f
Emerson Oliveira da Hora <emersondahora@gmail.com>
2020-06-04 02:45:59 -0300

    Incluído validação de dados de entrada

--- src/modules/products/infra/http/routes/admin.products.routes.ts
+++ src/modules/products/infra/http/routes/admin.products.routes.ts
@@ -6,6 +6,16 @@ import AdminProductsController from '../controllers/AdminProductsController';
 const adminProductsRoutes = Router();
 const adminProductsController = new AdminProductsController();
 
-adminProductsRoutes.post('/', adminProductsController.create);
+adminProductsRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      description: Joi.string().required(),
+      price: Joi.number().required(),
+    },
+  }),
+  adminProductsController.create,
+);
 
 export default adminProductsRoutes;
